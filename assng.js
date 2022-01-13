d7bf8872c835ffe68e1771455387de1d6026e14d
Lino Mathew <37393370+linomathew@users.noreply.github.com>
2022-01-13 11:27:39 +0530

    Add files via upload

---
+++ assng.js
@@ -0,0 +1,87 @@
+//Assignment 1
+
+var name= "Lino";
+var age= 24;
+var sts= "Single";
+console.log('Name :',name);
+console.log('Age :',age);
+console.log('Marital status :',sts);
+
+//Assignment 2
+
+var gender= "male";
+var height= 5.9;
+var preference= "chicken";
+console.log(typeof gender); 
+console.log(typeof height); 
+console.log(typeof preference);
+
+//Assignment 3
+
+var y= '2022';
+var yob= '1997';
+console.log('your age is :',y-yob);
+
+//Assignment 4
+
+var maths= 87;
+var eng= 90;
+var sci= 86;
+console.log('total marks =',maths+eng+sci);
+
+//Assignment 5
+
+var l= 25;
+var b= 15;
+console.log('the area of the rectangle is',l*b);
+console.log('the perimeter of the rectangle is',(2*l)+(2*b));
+
+//Assignment 6
+
+var m= 39;
+console.log('Total months =',m);
+console.log('Remaining months =',m%12);
+
+//Assignment 7
+
+var p= 150000;
+var i= 15;
+var t= 2;  
+var si= p*(i/100)*t;
+console.log('the simple interest rate is ',si);
+
+//Assignment 8
+
+var a= 85;
+var b= 75;
+var c= 78;
+var d= 80;
+var e= 65;
+var f= 70;
+var i= a+b+c+d+e+f;
+console.log('the averge mark =',i/6);
+
+//Assignment 9
+
+var p1= 100;
+var p2= 80;
+var p3= 90;
+var q1= 10;
+var q2= 20;
+var q3= 5;
+var t1= p1*q1;
+var t2= p2*q2;
+var t3= p3*q3;
+console.log('Total price =',t1+t2+t3);
+
+//Assignment 10 
+
+var p= 5000;
+var q= 10;
+var tax= 10;
+var d= 4500;
+var tip=100;
+var i= (p*q)-d;
+var j= i*(tax/100);
+console.log('tax amount =',j);
+console.log('the total payable amount = ',i+j+tip);
